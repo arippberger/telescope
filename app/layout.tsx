1b029b7c27737f96e2dda6a7bc2b338b2ea6aa88
Alec Rippberger <arippberger@gmail.com>
2023-02-12 13:26:28 -0600

    update config and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,30 @@ const syne = Syne({
   weight: ['400', '600', '800'],
   subsets: ['latin'],
   variable: '--font-syne',
+  display: 'swap',
 });
 
 const inter = Inter({
   weight: ['200', '400', '600', '700'],
   subsets: ['latin'],
   variable: '--font-inter',
+  display: 'swap',
 });
+
+const metadata = {
+  title: {
+    default: 'Telescope | Explore GitHub Stars',
+    template: '%s | Telescope | Explore GitHub Stars',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ['avif', 'webp', 'jpeg'],
+  },
   experimental: {
     appDir: true,
   },
