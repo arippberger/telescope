fce62150c4dc27836631093a3c52af81510305e2
Alec Rippberger <arippberger@gmail.com>
2023-02-16 21:20:21 -0600

    fix mobile navigation issue

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -72,6 +72,7 @@ export default function Nav() {
                     key={`${item.name}-${index}`}
                     href={item.href}
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
