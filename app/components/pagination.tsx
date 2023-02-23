9cdf3cedab6f83c3f3b1d76b9c2d49c7270f751a
Alec Rippberger <arippberger@gmail.com>
2023-02-22 21:24:20 -0600

    update pagination from anchors to buttons

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -26,8 +26,7 @@ export default function Pagination(props: Props) {
       <nav className="flex flex-1 items-center justify-between border-t border-gray-200 px-4 sm:px-0">
         {props.previousPage && (
           <div className="-mt-px flex w-0 flex-1">
-            <a
-              href="#"
+            <button
               onClick={handlePreviousPage}
               className="inline-flex items-center border-t-2 border-transparent pt-4 pr-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
             >
@@ -36,13 +35,12 @@ export default function Pagination(props: Props) {
                 aria-hidden="true"
               />
               Previous
-            </a>
+            </button>
           </div>
         )}
         {props.nextPage && (
           <div className="-mt-px flex w-0 flex-1 justify-end">
-            <a
-              href="#"
+            <button
               onClick={handleNextPage}
               className="inline-flex items-center border-t-2 border-transparent pt-4 pl-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
             >
@@ -51,7 +49,7 @@ export default function Pagination(props: Props) {
                 className="ml-3 h-5 w-5 text-gray-400"
                 aria-hidden="true"
               />
-            </a>
+            </button>
           </div>
         )}
       </nav>
